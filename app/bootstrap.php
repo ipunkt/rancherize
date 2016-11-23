9eadd1b24dc8c4a51037a3d6d89787d75b4ca09e
Sven Speckmaier <sps@ipunkt.biz>
2016-11-23 20:11:02 +0100

    autoloading from within the vendor directory

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,10 @@
 <?php
 
-require __DIR__.'/../vendor/autoload.php';
+if( file_exists(__DIR__.'/../vendor/autoload.php') ) {
+	require __DIR__.'/../vendor/autoload.php';
+} else {
+	// inside vendor directory. app -> rancherize -> vendor
+	require __DIR__.'/../../../autoload.php';
+}
 require __DIR__.'/container.php';
 
