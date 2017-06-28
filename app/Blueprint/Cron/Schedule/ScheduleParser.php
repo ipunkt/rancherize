8d6fe2a2a2fe534854d914eb4bb8d0f3399208b2
Sven Speckmaier <sps@ipunkt.biz>
2017-06-28 17:02:04 +0200

    added schedule parsing

--- app/Blueprint/Cron/Schedule/ScheduleParser.php
+++ app/Blueprint/Cron/Schedule/ScheduleParser.php
@@ -16,6 +16,25 @@ class ScheduleParser {
 		$schedule = new Schedule();
 
 		$scheduleString = $configuration->get('schedule');
+		if( is_string($scheduleString) ) {
+			$parts = explode(' ', $scheduleString);
+
+			$schedule->setSeconds( $parts[0] );
+			$schedule->setMinute( $parts[1] );
+			$schedule->setHour( $parts[2] );
+			$schedule->setMonth( $parts[3] );
+			$schedule->setDayOfMonth( $parts[4] );
+			$schedule->setDayOfWeek( $parts[5] );
+
+			return $schedule;
+		}
+
+		$schedule->setHour( $configuration->get('hour', '*') );
+		$schedule->setMinute( $configuration->get('minute', '*') );
+		$schedule->setSeconds( $configuration->get('seconds', '*') );
+		$schedule->setMonth( $configuration->get('hour', '*') );
+		$schedule->setDayOfMonth( $configuration->get('dayOfMonth', '*') );
+		$schedule->setDayOfWeek( $configuration->get('dayOfWeek', '*') );
 
 		return $schedule;
 	}
