4f7432c27558896c1bc9dfe34861de3d9539e12c
Sven Speckmaier <sps@ipunkt.biz>
2017-08-22 17:08:59 +0200

    changed webserver blueprint image version to 1.3.1

--- app/Blueprint/Webserver/WebserverBlueprint.php
+++ app/Blueprint/Webserver/WebserverBlueprint.php
@@ -314,9 +314,9 @@ class WebserverBlueprint implements Blueprint, TakesDockerAccount {
 	protected function makeServerService(Configuration $config, Configuration $default) : Service {
 		$serverService = new Service();
 		$serverService->setName($config->get('service-name'));
-		$serverService->setImage($config->get('docker.image', 'ipunktbs/nginx:1.10.2-7-1.3.0'));
+		$serverService->setImage($config->get('docker.image', 'ipunktbs/nginx:1.10.2-7-1.3.1'));
 		if( $config->get('debug-image', false) )
-			$serverService->setImage($config->get('docker.image', 'ipunktbs/nginx-debug:debug-1.3.0'));
+			$serverService->setImage($config->get('docker.image', 'ipunktbs/nginx-debug:debug-1.3.1'));
 
 		if( $config->get('sync-user-into-container', false) ) {
 			$serverService->setEnvironmentVariable('USER_ID',empty($_ENV['USER_ID']) ? getmyuid() : $_ENV['USER_ID'] );
