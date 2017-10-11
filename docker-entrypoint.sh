b83df0b977548fc208c3a36807ef27fc3e3063f6
Bastian Rang <br@ipunkt.biz>
2017-10-11 15:53:56 +0200

    this refs #34 and should allow integration in gitlab-ci now

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-if [ "$1" = "sh" ] ; then
-	shift 1
-fi
+export PATH=$PATH:/opt/rancherize
 
 if type "$1" >/dev/null ; then
-	exec $*
+	exec "$@"
 	exit $?
 fi
 
-exec php /opt/rancherize/rancherize $@
+exec rancherize "$@"
