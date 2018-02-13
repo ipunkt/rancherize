5be6e898e410beadadb4b754fd30c7326da36bc4
Sven Speckmaier <sps@ipunkt.biz>
2018-02-13 16:45:19 +0100

    fixed Configuration::has on non-array and Cron container entries

--- app/Blueprint/Cron/CronProvider.php
+++ app/Blueprint/Cron/CronProvider.php
@@ -1,5 +1,6 @@
 <?php namespace Rancherize\Blueprint\Cron;
 
+use Illuminate\Console\Scheduling\Schedule;
 use Rancherize\Blueprint\Cron\CronInit\CronInit;
 use Rancherize\Blueprint\Cron\CronParser\CronParser;
 use Rancherize\Blueprint\Cron\CronService\CronService;
@@ -18,20 +19,36 @@ class CronProvider implements Provider {
 	/**
 	 */
 	public function register() {
-		$this->container['cron-init'] = function() {
+		$this->container[CronInit::class] = function() {
 			return new CronInit();
 		};
+		$this->container['cron-init'] = function($c) {
+			return $c[CronInit::class];
+		};
 
-		$this->container['schedule-parser'] = function($c) {
+		$this->container[ScheduleParser::class] = function($c) {
 			return new ScheduleParser();
 		};
 
-		$this->container['cron-service'] = function($c) {
+		$this->container['schedule-parser'] = function($c) {
+			return $c[ScheduleParser::class];
+		};
+
+		$this->container[CronService::class] = function($c) {
 			return new CronService();
 		};
 
-		$this->container['cron-parser'] = function($c) {
+		$this->container['cron-service'] = function($c) {
+			return $c[CronService::class];
+		};
+
+
+		$this->container[CronParser::class] = function($c) {
 			return new CronParser( $c['schedule-parser'], $c['cron-service']);
+		};
+
+		$this->container['cron-parser'] = function($c) {
+			return $c[CronParser::class];
 		};
 	}
 
