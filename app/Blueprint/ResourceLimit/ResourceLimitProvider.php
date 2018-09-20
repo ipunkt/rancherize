602b4e0f3039051024e1c4bc0fe2c2ecb305c9c4
Sven Speckmaier <sps@ipunkt.biz>
2018-09-20 14:40:15 +0200

    mem limits

--- app/Blueprint/ResourceLimit/ResourceLimitProvider.php
+++ app/Blueprint/ResourceLimit/ResourceLimitProvider.php
@@ -5,9 +5,15 @@ use Rancherize\Blueprint\Infrastructure\Service\Events\ServiceWriterServicePrepa
 use Rancherize\Blueprint\ResourceLimit\EventListener\MainServiceBuiltListener;
 use Rancherize\Blueprint\ResourceLimit\EventListener\ServiceWriteListener;
 use Rancherize\Blueprint\ResourceLimit\Parser\CpuLimitModeFactory;
+use Rancherize\Blueprint\ResourceLimit\Parser\MemLimitModeFactory;
+use Rancherize\Blueprint\ResourceLimit\Parser\MemModes\FullMemMode;
+use Rancherize\Blueprint\ResourceLimit\Parser\MemModes\HighMemMode;
+use Rancherize\Blueprint\ResourceLimit\Parser\MemModes\LowMemMode;
+use Rancherize\Blueprint\ResourceLimit\Parser\MemModes\MinimalMemMode;
 use Rancherize\Blueprint\ResourceLimit\Parser\Modes\FullCpuMode;
 use Rancherize\Blueprint\ResourceLimit\Parser\Modes\HighCpuMode;
 use Rancherize\Blueprint\ResourceLimit\Parser\Modes\LowCpuMode;
+use Rancherize\Blueprint\ResourceLimit\Parser\Modes\MinimalCpuMode;
 use Rancherize\Blueprint\ResourceLimit\Parser\Parser;
 use Rancherize\Plugin\Provider;
 use Rancherize\Plugin\ProviderTrait;
@@ -45,12 +51,48 @@ class ResourceLimitProvider implements Provider {
 			return $c[LowCpuMode::class];
 		};
 
+		$this->container[MinimalCpuMode::class] = function () {
+			return new MinimalCpuMode();
+		};
+		$this->container['resource-limit.cpu-limit.minimal'] = function ( $c ) {
+			return $c[MinimalCpuMode::class];
+		};
+
 		$this->container[CpuLimitModeFactory::class] = function ( $c ) {
 			return new CpuLimitModeFactory( $c );
 		};
 
+		$this->container[FullMemMode::class] = function () {
+			return new FullMemMode();
+		};
+		$this->container['resource-limit.mem-limit.full'] = function ( $c ) {
+			return $c[FullMemMode::class];
+		};
+		$this->container[HighMemMode::class] = function () {
+			return new HighMemMode();
+		};
+		$this->container['resource-limit.mem-limit.high'] = function ( $c ) {
+			return $c[HighMemMode::class];
+		};
+		$this->container[LowMemMode::class] = function () {
+			return new LowMemMode();
+		};
+		$this->container['resource-limit.mem-limit.low'] = function ( $c ) {
+			return $c[LowMemMode::class];
+		};
+		$this->container[MinimalMemMode::class] = function () {
+			return new MinimalMemMode();
+		};
+		$this->container['resource-limit.mem-limit.minimal'] = function ( $c ) {
+			return $c[MinimalMemMode::class];
+		};
+
+		$this->container[MemLimitModeFactory::class] = function($c) {
+			return new MemLimitModeFactory( $c );
+		};
+
 		$this->container[Parser::class] = function ( $c ) {
-			return new Parser( $c[CpuLimitModeFactory::class] );
+			return new Parser( $c[CpuLimitModeFactory::class], $c[MemLimitModeFactory::class] );
 		};
 
 		$this->container[ServiceWriteListener::class] = function () {
