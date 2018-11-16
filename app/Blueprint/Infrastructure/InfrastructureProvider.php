d23f7cc842aca3e728d6ae6c5bb3db7656f6db19
Sven Speckmaier <sps@ipunkt.biz>
2018-11-16 11:25:24 +0100

    refs #96 add ServiceCopier to ServiceWriter

--- app/Blueprint/Infrastructure/InfrastructureProvider.php
+++ app/Blueprint/Infrastructure/InfrastructureProvider.php
@@ -1,5 +1,6 @@
 <?php namespace Rancherize\Blueprint\Infrastructure;
 
+use Rancherize\Blueprint\Infrastructure\Copier\ServiceCopier;
 use Rancherize\Blueprint\Infrastructure\Dockerfile\DockerfileWriter;
 use Rancherize\Blueprint\Infrastructure\Network\NetworkWriter;
 use Rancherize\Blueprint\Infrastructure\Service\Listeners\AlwaysPullDefaultFromConfigurationListener;
@@ -22,87 +23,92 @@ use Symfony\Component\EventDispatcher\EventDispatcher;
  */
 class InfrastructureProvider implements Provider {
 
-	use ProviderTrait;
+    use ProviderTrait;
 
-	/**
-	 */
-	public function register() {
-		$this->container[DockerfileWriter::class] = function() {
-			return new DockerfileWriter();
-		};
-		$this->container['dockerfile-writer'] = function($c) {
-			return $c[DockerfileWriter::class];
-		};
+    /**
+     */
+    public function register() {
+        $this->container[DockerfileWriter::class] = function() {
+            return new DockerfileWriter();
+        };
+        $this->container['dockerfile-writer'] = function($c) {
+            return $c[DockerfileWriter::class];
+        };
 
-		$this->container[ServiceWriter::class] = function($c) {
-			return new ServiceWriter($c[FileLoader::class], $c['event'], $c[DockerfileWriter::class]);
-		};
-		$this->container['service-writer'] = function($c) {
-			return $c[ServiceWriter::class];
-		};
+        $this->container[ServiceWriter::class] = function($c) {
+            return new ServiceWriter($c[FileLoader::class], $c['event'], $c[DockerfileWriter::class]);
+        };
+        $this->container['service-writer'] = function($c) {
+            return $c[ServiceWriter::class];
+        };
 
-		$this->container[VolumeWriter::class] = function($c) {
-			return new VolumeWriter($c[FileLoader::class]);
-		};
-		$this->container['volume-writer'] = function($c) {
-			return $c[VolumeWriter::class];
-		};
+        $this->container[VolumeWriter::class] = function($c) {
+            return new VolumeWriter($c[FileLoader::class]);
+        };
+        $this->container['volume-writer'] = function($c) {
+            return $c[VolumeWriter::class];
+        };
 
-		$this->container[NetworkWriter::class] = function($c) {
-			return new NetworkWriter( $c[FileLoader::class] );
-		};
+        $this->container[NetworkWriter::class] = function($c) {
+            return new NetworkWriter( $c[FileLoader::class] );
+        };
 
-		$this->container[\Rancherize\Blueprint\Infrastructure\InfrastructureWriter::class] = function($c) {
-			return new \Rancherize\Blueprint\Infrastructure\InfrastructureWriter(
-				$c[DockerfileWriter::class],
-				$c[ServiceWriter::class],
-				$c[VolumeWriter::class],
-				$c[NetworkWriter::class]);
-		};
+        $this->container[ServiceCopier::class] = function () {
+            return new ServiceCopier();
+        };
 
-		$this->container['infrastructure-writer'] = function($c) {
-			return $c[\Rancherize\Blueprint\Infrastructure\InfrastructureWriter::class];
-		};
+        $this->container[\Rancherize\Blueprint\Infrastructure\InfrastructureWriter::class] = function($c) {
+            return new \Rancherize\Blueprint\Infrastructure\InfrastructureWriter(
+                $c[DockerfileWriter::class],
+                $c[ServiceWriter::class],
+                $c[VolumeWriter::class],
+                $c[NetworkWriter::class],
+                $c[ServiceCopier::class]);
+        };
 
-		$this->container[AlwaysPullDefaultFromConfigurationListener::class] = function ( $c ) {
-			return new AlwaysPullDefaultFromConfigurationListener( $c );
-		};
+        $this->container['infrastructure-writer'] = function($c) {
+            return $c[\Rancherize\Blueprint\Infrastructure\InfrastructureWriter::class];
+        };
 
-		$this->container['docker-compose.version'] = '2';
-		$this->container['shared-network-mode'] = 'container:';
-		$this->container['always-pulled-default'] = Service::ALWAYS_PULLED_TRUE;
+        $this->container[AlwaysPullDefaultFromConfigurationListener::class] = function ( $c ) {
+            return new AlwaysPullDefaultFromConfigurationListener( $c );
+        };
 
-	}
+        $this->container['docker-compose.version'] = '2';
+        $this->container['shared-network-mode'] = 'container:';
+        $this->container['always-pulled-default'] = Service::ALWAYS_PULLED_TRUE;
 
-	/**
-	 */
-	public function boot() {
-		/**
-		 * @var EventDispatcher $dispatcher
-		 */
-		$dispatcher = container('event');
-		$dispatcher->addListener(ConsoleEvents::COMMAND, function( ConsoleCommandEvent $event ) {
+    }
 
-			$command = $event->getCommand();
-			if( $command instanceof LocalCommand ) {
-				$container = container();
-				$container['shared-network-mode'] = 'service:';
+    /**
+     */
+    public function boot() {
+        /**
+         * @var EventDispatcher $dispatcher
+         */
+        $dispatcher = container('event');
+        $dispatcher->addListener(ConsoleEvents::COMMAND, function( ConsoleCommandEvent $event ) {
+
+            $command = $event->getCommand();
+            if( $command instanceof LocalCommand ) {
+                $container = container();
+                $container['shared-network-mode'] = 'service:';
                 $container['docker-compose.version'] = '2.1';
 
-				return;
-			}
+                return;
+            }
 
-			if( $command instanceof RancherCommand ) {
-				// shared-network-mode defaults to 'container:' in the register function
-				return;
-			}
-		});
+            if( $command instanceof RancherCommand ) {
+                // shared-network-mode defaults to 'container:' in the register function
+                return;
+            }
+        });
 
-		$listener = $this->container[AlwaysPullDefaultFromConfigurationListener::class];
-		/**
-		 * @var EventDispatcher $event
-		 */
-		$event = $this->container['event'];
-		$event->addListener( EnvironmentConfigurationLoadedEvent::NAME, [$listener, 'environmentConfigurationLoaded'] );
-	}
+        $listener = $this->container[AlwaysPullDefaultFromConfigurationListener::class];
+        /**
+         * @var EventDispatcher $event
+         */
+        $event = $this->container['event'];
+        $event->addListener( EnvironmentConfigurationLoadedEvent::NAME, [$listener, 'environmentConfigurationLoaded'] );
+    }
 }
