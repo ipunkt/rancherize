ed982cce27216c8ac165f53168a060de057462d8
Sven Speckmaier <sps@ipunkt.biz>
2018-12-13 13:27:49 +0100

    copy volumes in the ServiceCopier

--- app/Blueprint/Infrastructure/Copier/ServiceCopier.php
+++ app/Blueprint/Infrastructure/Copier/ServiceCopier.php
@@ -1,6 +1,7 @@
 <?php namespace Rancherize\Blueprint\Infrastructure\Copier;
 
 use Rancherize\Blueprint\Infrastructure\Service\Service;
+use Rancherize\Blueprint\Infrastructure\Service\Volume;
 
 /**
  * Class ServiceCopier
@@ -22,6 +23,14 @@ class ServiceCopier
         $newService->setRestart($service->getRestart());
         $newService->setCommand($service->getCommand());
         $newService->setKeepStdin($service->isKeepStdin());
+	    foreach ( $service->getVolumeObjects() as $volumeObject ) {
+	    	$newVolume = new Volume();
+	    	$newVolume->setDriver($volumeObject->getDriver());
+		    $newVolume->setExternalPath($volumeObject->getExternalPath());
+		    $newVolume->setInternalPath($volumeObject->getInternalPath());
+		    $newVolume->setMountOptions($volumeObject->getMountOptions());
+		    $newVolume->setOptions($volumeObject->getOptions());
+        }
 
         return $newService;
     }
