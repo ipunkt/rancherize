3b842f5ec018c2e319d548f74aa99cce28941703
rok <rok@ipunkt.biz>
2019-05-08 15:06:25 +0200

    whitespace

--- app/Blueprint/Infrastructure/Service/Services/LaravelQueueWorker.php
+++ app/Blueprint/Infrastructure/Service/Services/LaravelQueueWorker.php
@@ -8,38 +8,38 @@ use Rancherize\Blueprint\Infrastructure\Service\Service;
  */
 class LaravelQueueWorker extends Service
 {
-	const DEFAULT_IMAGE_VERSION = 'php7.0-v1.0';
+    const DEFAULT_IMAGE_VERSION = 'php7.0-v1.0';
 
-	private $__validImageVersions = [
-		'php7.0-v1.0',
-		'php7.1-v2.0',
-		'php7.2-v3.0',
+    private $__validImageVersions = [
+        'php7.0-v1.0',
+        'php7.1-v2.0',
+        'php7.2-v3.0',
         'php7.3-v4.0',
-		'latest',
-	];
+        'latest',
+    ];
 
-	public function __construct($imageVersion = null)
-	{
-		parent::__construct();
-		$this->setName('LaravelQueueWorker');
-		$this->setImageVersion($imageVersion);
-		$this->setRestart(self::RESTART_UNLESS_STOPPED);
+    public function __construct($imageVersion = null)
+    {
+        parent::__construct();
+        $this->setName('LaravelQueueWorker');
+        $this->setImageVersion($imageVersion);
+        $this->setRestart(self::RESTART_UNLESS_STOPPED);
 
 //		$this->setTty(true);
 //		$this->setKeepStdin(true);
-	}
+    }
 
-	public function setImageVersion($version)
-	{
-		if ($version === null) {
-			$this->setImage('ipunktbs/laravel-queue-worker:' . self::DEFAULT_IMAGE_VERSION);
-			return;
-		}
+    public function setImageVersion($version)
+    {
+        if ($version === null) {
+            $this->setImage('ipunktbs/laravel-queue-worker:' . self::DEFAULT_IMAGE_VERSION);
+            return;
+        }
 
-		if (!in_array($version, $this->__validImageVersions, false)) {
-			return;
-		}
+        if (!in_array($version, $this->__validImageVersions, false)) {
+            return;
+        }
 
-		$this->setImage('ipunktbs/laravel-queue-worker:' . $version);
-	}
+        $this->setImage('ipunktbs/laravel-queue-worker:' . $version);
+    }
 }
