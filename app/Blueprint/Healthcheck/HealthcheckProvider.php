4f8263f74632749dbb875d759d50e45f01ce5844
svensp <sps@ipunkt.biz>
2020-01-07 17:03:45 +0100

    healthcheck - register with classpathes

--- app/Blueprint/Healthcheck/HealthcheckProvider.php
+++ app/Blueprint/Healthcheck/HealthcheckProvider.php
@@ -20,17 +20,32 @@ class HealthcheckProvider implements Provider {
 	/**
 	 */
 	public function register() {
-		$this->container['healthcheck-yaml-writer'] = function() {
-			return new HealthcheckYamlWriter();
-		};
+        $this->container[HealthcheckYamlWriter::class] = function() {
+            return new HealthcheckYamlWriter();
+        };
+        $this->container['healthcheck-yaml-writer'] = function($c) {
+            return $c[HealthcheckYamlWriter::class];
+        };
+
+        $this->container[HealthcheckServiceWriterListener::class] = function ($c) {
+            return new HealthcheckServiceWriterListener( $c[HealthcheckYamlWriter::class] );
+        };
 		$this->container['healthcheck-service-writer-listener'] = function ($c) {
-			return new HealthcheckServiceWriterListener( $c['healthcheck-yaml-writer'] );
+			return $c[HealthcheckServiceWriterListener::class];
 		};
-		$this->container['healthcheck-default-setter'] = function() {
-			return new HealthcheckDefaultInformationSetter;
+
+        $this->container[HealthcheckDefaultInformationSetter::class] = function() {
+            return new HealthcheckDefaultInformationSetter;
+        };
+		$this->container['healthcheck-default-setter'] = function($c) {
+			return $c[HealthcheckDefaultInformationSetter::class];
 		};
+
+        $this->container[HealthcheckConfigurationToService::class] = function ($c) {
+            return new HealthcheckConfigurationToService( $c['healthcheck-default-setter'] );
+        };
 		$this->container['healthcheck-parser'] = function ($c) {
-			return new HealthcheckConfigurationToService( $c['healthcheck-default-setter'] );
+			return $c[HealthcheckConfigurationToService::class];
 		};
 	}
 
